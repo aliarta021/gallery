79e39e76ee23e8538aace02de389f2f330ba6fc5
alireza <aliarta021@gmail.com>
2023-01-17 18:39:19 +0330

    [ADD] sliver list

--- lib/app.dart
+++ lib/app.dart
@@ -11,6 +11,7 @@ import 'package:revolution1401/common/styles/appTheme/app_theme.dart';
 import 'package:revolution1401/common/styles/colorPalette/color_palette.dart';
 import 'package:revolution1401/common/uikit/clickable/push_down_clickable.dart';
 import 'package:revolution1401/common/utils/app_scroll_behavior.dart';
+import 'package:revolution1401/modules/home/bloc/home_bloc.dart';
 import 'package:revolution1401/modules/main/bloc/main_page_bloc.dart';
 import 'package:revolution1401/modules/main/ui/pages/splash_page.dart';
 
--- lib/modules/home/ui/pages/home_page.dart
+++ lib/modules/home/ui/pages/home_page.dart
@@ -1,9 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:revolution1401/common/uikit/search_widget.dart';
 import 'package:revolution1401/common/utils/status_bar.dart';
 import 'package:revolution1401/modules/home/bloc/home_bloc.dart';
+import 'package:revolution1401/modules/home/ui/widgets/slider/image_banner.dart';
 import 'package:revolution1401/modules/home/ui/widgets/slider/simple_banner_slider.dart';
 import 'package:revolution1401/modules/main/ui/widgets/header_widget.dart';
 
@@ -24,7 +26,16 @@ class HomePage extends StatelessWidget {
                     },
                   ),
                 ),
-                SimpleBannerSlider(context.read<HomeBloc>().list),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40, bottom: 30),
+                        child: SimpleBannerSlider(context.read<HomeBloc>().list),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
